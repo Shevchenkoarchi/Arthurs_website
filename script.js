651ab8b63d3d654e6a6343725247431bbc74fef8
arturshevchenko <shevchenkoarchi@gmail.com>
2023-03-20 19:38:06 -0500

    add image

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const header = document.createElement("header");
+header.textContent = "My top anime list"
+document.querySelector("body").appendChild(header);
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
